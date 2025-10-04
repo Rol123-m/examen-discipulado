b654b04ca32f1a777315221e6d43dd7e79e1ad41
Rol123-m <rol123-m.github.io.com>
2025-10-04 16:27:48 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(7)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="src/hi.html" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Como te va mijito {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Aprende un poquito más
       </p>
     </>
   )
